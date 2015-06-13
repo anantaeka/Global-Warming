731c66d2bf15481c9c71454401fa8c5b8bf1933c
Ananta Eka <131112349@students.mikroskil.ac.id>
2015-06-13 22:54:46 +0700

    Ganti Theme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
-  cube = require('bespoke-theme-cube'),
+  greeny = require('bespoke-theme-greeny'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
@@ -12,7 +12,8 @@ var bespoke = require('bespoke'),
 
 // Bespoke.js
 bespoke.from('article', [
-  cube(),
+  greeny.theme(),
+  greeny.scale(),
   keys(),
   touch(),
   bullets('li, .bullet'),
